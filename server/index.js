7c6acfebb7e6b13c47604dfd6ee1ba18cefb6506
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-11-14 14:35:14 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('endpoint live')
+});
+
+const PORT = 4001;
+app.listen(PORT, () => console.log(`Server listening on port PORT`));
