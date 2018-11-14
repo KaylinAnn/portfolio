44733736fabd02c88ed3c3d03f910b18b9054a23
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-11-14 15:22:25 -0700

    added styling to intro

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Intro.css";
 
 export class Intro extends Component {
   render() {
     return (
-      <div>
-        <h1>KAYLIN'S PORTFOLIO</h1>
-        <h2>Hi there! I'm a Full-stack Web Developer who loves creating new and exciting sites.</h2>
+      <div className="intro">
+        <h2>KAYLIN'S PORTFOLIO</h2>
+        <div className="message-container">
+          <h1>
+            Hi there! I'm a Full-stack Web Developer who loves creating new and
+            exciting sites.
+          </h1>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Intro
+export default Intro;
