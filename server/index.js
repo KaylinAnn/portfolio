93aa42452d4b1aee97a6ecc677f1f3bda5397627
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2018-11-19 17:17:44 -0700

    added nodemailer and styled contact page

--- server/index.js
+++ server/index.js
@@ -1,11 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('endpoint live')
+var transport = {
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS
+  }
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("endpoint live");
+});
+
+app.post("/api/send", (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const content = `name: ${name} \n email: ${email} \n message: ${message} `;
+
+  const mail = {
+    from: name,
+    to: "kaylinann89@gmail.com", //Change to email address that you want to receive messages on
+    subject: "New Message from Contact Form",
+    text: content
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    console.log(data);
+
+    if (err) {
+      res.json({
+        msg: "fail"
+      });
+    } else {
+      res.json({
+        msg: "success"
+      });
+    }
+  });
 });
 
 const PORT = 4001;
--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -1,9 +1,74 @@
 import React, { Component } from "react";
 import "./ContactMe.css";
+import axios from "axios";
 
 export class ContactMe extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    if (name === "" || email === "" || message === "") {
+      alert("Oh no! One or more inputs are empty.");
+    } else {
+      axios
+        .post("/api/send", {
+          name: name,
+          email: email,
+          message: message
+        })
+        .then(response => {
+          console.log(response.data);
+
+          if (response.data.msg === "success") {
+            alert("Message Sent.");
+            this.resetForm();
+          } else if (response.data.msg === "fail") {
+            alert("Message failed to send.");
+          }
+        });
+    }
+  }
+
+  resetForm() {
+    document.getElementById("contact-form").reset();
+  }
+
   render() {
-    return <div className="contact-me">blah</div>;
+    return (
+      <div className="contact-me">
+        <div className="contact-text-container">
+          <h1 className="contact-text">Contact Me</h1>
+        </div>
+
+        <form
+          className="contact-form-container"
+          id="contact-form"
+          onSubmit={this.handleSubmit.bind(this)}
+        >
+          <div className="name-input-container input-container">
+            <label for="name">Name</label>
+            <input type="text" className="form-control" id="name" />
+          </div>
+          <div className="email-input-container input-container">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="message-input-container input-container">
+            <label for="message">Message</label>
+            <textarea className="form-control-message" rows="5" id="message" />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            SUBMIT
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
